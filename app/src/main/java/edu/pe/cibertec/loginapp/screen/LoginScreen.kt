1a06ce76b9ff596ea3ceee3b26071ffb5d21c0f7
SUITE <DW1@mail.com>
2025-05-26 16:04:16 -0500

    mutable for onchange value on each OutlinedTextField

--- app/src/main/java/edu/pe/cibertec/loginapp/screen/LoginScreen.kt
+++ app/src/main/java/edu/pe/cibertec/loginapp/screen/LoginScreen.kt
@@ -4,12 +4,18 @@ import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -21,9 +27,13 @@ import androidx.compose.ui.unit.dp
 )
 @Composable
 fun  LoginScreen(modifier: Modifier = Modifier){
-    var userName:String = ""
-    var password: String = ""
-    Column (modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center)
+    var userName by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Column (modifier = modifier.fillMaxSize().padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+
+    )
     {
         Text(
             "Usuario",
@@ -31,7 +41,7 @@ fun  LoginScreen(modifier: Modifier = Modifier){
         )
         Spacer(modifier= Modifier.height(15.dp))
         OutlinedTextField(
-            value = "",
+            value = userName,
             onValueChange =  {
                 userName = it.toString()
             },
@@ -47,7 +57,7 @@ fun  LoginScreen(modifier: Modifier = Modifier){
         )
         Spacer(modifier= Modifier.height(15.dp))
         OutlinedTextField(
-            value = "",
+            value = password,
             onValueChange = {
                 password = it.toString()
             },
@@ -60,7 +70,7 @@ fun  LoginScreen(modifier: Modifier = Modifier){
         Spacer(modifier= Modifier.height(15.dp))
         Button(
             onClick = {
-
+                Log.d("LOGIN OK!", userName)
             },
             modifier = Modifier.fillMaxWidth()
         ) {
