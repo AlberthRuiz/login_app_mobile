e3c29e9f17d8b69b9cf44e9008c4c57e05048c1c
SUITE <DW1@mail.com>
2025-05-26 15:48:26 -0500

    Complete Login UI

--- app/src/main/java/edu/pe/cibertec/loginapp/MainActivity.kt
+++ app/src/main/java/edu/pe/cibertec/loginapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import edu.pe.cibertec.loginapp.screen.LoginScreen
 import edu.pe.cibertec.loginapp.ui.theme.LoginAPPTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LoginAPPTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LoginScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
--- app/src/main/java/edu/pe/cibertec/loginapp/screen/LoginScreen.kt
+++ app/src/main/java/edu/pe/cibertec/loginapp/screen/LoginScreen.kt
@@ -6,11 +6,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -35,8 +37,35 @@ fun  LoginScreen(modifier: Modifier = Modifier){
             },
             label = {
                 Text("Usuario aqui")
-            }
+            },
+            modifier = Modifier.fillMaxWidth()
         )
+        Spacer(modifier= Modifier.height(15.dp))
+        Text(
+            "Password",
+            modifier = modifier.fillMaxWidth()
+        )
+        Spacer(modifier= Modifier.height(15.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = {
+                password = it.toString()
+            },
+            label = {
+                Text("Password Aqui")
+            },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier= Modifier.height(15.dp))
+        Button(
+            onClick = {
+
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Iniciar Sesion")
+        }
 
 
     }
