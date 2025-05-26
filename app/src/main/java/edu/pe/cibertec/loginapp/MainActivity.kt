61605548c4cccbfcf1f493babb174e2331059994
SUITE <DW1@mail.com>
2025-05-26 15:29:54 -0500

    Initial Commit

---
+++ app/src/main/java/edu/pe/cibertec/loginapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.pe.cibertec.loginapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.pe.cibertec.loginapp.ui.theme.LoginAPPTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoginAPPTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LoginAPPTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/edu/pe/cibertec/loginapp/screen/LoginScreen.kt
@@ -0,0 +1,44 @@
+package edu.pe.cibertec.loginapp.screen
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun  LoginScreen(modifier: Modifier = Modifier){
+    var userName:String = ""
+    var password: String = ""
+    Column (modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center)
+    {
+        Text(
+            "Usuario",
+            modifier = modifier.fillMaxWidth()
+        )
+        Spacer(modifier= Modifier.height(15.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange =  {
+                userName = it.toString()
+            },
+            label = {
+                Text("Usuario aqui")
+            }
+        )
+
+
+    }
+
+}
